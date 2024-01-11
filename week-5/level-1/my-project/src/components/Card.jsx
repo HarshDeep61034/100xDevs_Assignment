a91271940c3c60b3a917b8e43a0b2cae4fc8a342
Harsh Deep <hdeep61034@gmail.com>
2024-01-11 22:57:59 +0530

    solving week-5 assignment

--- week-5/level-1/my-project/src/components/Card.jsx
+++ week-5/level-1/my-project/src/components/Card.jsx
@@ -1,31 +1,33 @@
 const Card = ({ image, name, title, description, socials, intrests }) => {
   return (
-    <div>
+    <div className="m-3 p-3 border-black border-2 rounded-lg w-72 flex flex-col items-center">
       <div>
-        <img src={image} />
-        <p>
-          Hi, I'm <span>{name}</span>
+        <img src={image} className="rounded-[50%] w-44" />
+        <p className="my-2 font-semibold text-2xl">
+          Hi, I'm <span className="text-blue-600">{name}</span>
         </p>
-        <p>{title}</p>
+        <p className="text-center ">{title}</p>
       </div>
-      {/* <div> */}
-      {/*   <p>{description}</p> */}
-      {/*   <ul> */}
-      {/*     <p> */}
-      {/*       {intrests.map((intrest) => { */}
-      {/*         <li>{intrest}</li>; */}
-      {/*       })} */}
-      {/*     </p> */}
-      {/*   </ul> */}
-      {/*   <div> */}
-      {/*     {socials.map((item) => { */}
-      {/*       <a href={`https://${item.app}.com/${item.username}`}> */}
-      {/*         {item.app} */}
-      {/*         <button></button> */}
-      {/*       </a>; */}
-      {/*     })} */}
-      {/*   </div> */}
-      {/* </div> */}
+      <div>
+        <p>{description}</p>
+        <span className="text-xl font-bold">Hobbies: </span>
+        <ul>
+          {intrests.map((elem) => (
+            <li key={elem} className="text-black font-semibold text-md">
+              {elem}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="my-1">
+        {socials.map((item, index) => (
+          <a key={index} href={`https://${item.app}.com/${item.username}`}>
+            <button className="py-1 px-3 bg-blue-600 rounded-xl text-white mx-1">
+              {item.app}
+            </button>
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
