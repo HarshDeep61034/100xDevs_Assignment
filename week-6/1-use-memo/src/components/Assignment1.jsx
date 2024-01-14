1ab958eaa65cad36e9c092c33acc191ea9dd3367
Harsh Deep <hdeep61034@gmail.com>
2024-01-14 23:38:47 +0530

    assignment 1 of week-6 usememo done

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -6,11 +6,13 @@ import { useMemo, useState } from "react";
 export function Assignment1() {
   const [input, setInput] = useState(0);
   // Your solution starts here
-  let expensiveValue;
-  useMemo(() => {
+  const expensivealue = useMemo(() => {
+    let fact = 1;
     for (let i = 1; i <= input; i++) {
-      expensiveValue *= i;
+      fact *= i;
     }
+    console.log(fact);
+    return fact;
   }, [input]);
   // Your solution ends here
 
@@ -19,10 +21,11 @@ export function Assignment1() {
       <input
         type="number"
         value={input}
-        onChange={(e) => setInput(Number(e.target.value))}
+        onChange={(e) => {
+          setInput(Number(e.target.value));
+        }}
       />
-      <p>Calculated Value: {expensiveValue}</p>
+      <p>Calculated Value: {expensivealue}</p>
     </div>
   );
 }
-
